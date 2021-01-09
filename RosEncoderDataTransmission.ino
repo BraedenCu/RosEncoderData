30ed21257626050f12743102871dda7cd9a198b0
BraedenCu <braedencullen120@gmail.com>
2021-01-08 20:51:41 -0600

    ros publisher

--- RosEncoderDataTransmission.ino
+++ RosEncoderDataTransmission.ino
@@ -1,12 +1,11 @@
 #include <ros.h>
-#include <std_msgs/String.h>
-#include "Arduino.h"
+#include <std_msgs/float64.h>
+#include <nav_msgs/Odometry.h>
 #include <digitalWriteFast.h>  // library for high performance reads and writes by jrraines
                                // see http://www.arduino.cc/cgi-bin/yabb2/YaBB.pl?num=1267553811/0
                                // and http://code.google.com/p/digitalwritefast/
 //All credits for the fantastic quaternion encoder logic go to Daniel J. Gonzalez (http://yameb.blogspot.com/2012/11/quadrature-encoders-in-arduino-done.html)
                                
-
 // Quadrature encoders
 // Left encoder
 #define c_LeftEncoderInterruptA 0
@@ -21,10 +20,16 @@ volatile bool _LeftEncoderAPrev;
 volatile bool _LeftEncoderBPrev;
 volatile long _LeftEncoderTicks = 0;
 
+ros::NodeHandle nh;
+
+std_msgs::float64 float_msg;
+ros::Publisher encoder("encoder", &float_msg);
+
 void setup()
 {
   Serial.begin(9600);
-
+  nh.initNode()j;
+  nh.advertise(encoder);
   // Quadrature encoders
   // Left encoder
   pinMode(c_LeftEncoderPinA, INPUT);      // sets pin A as input
@@ -37,11 +42,14 @@ void setup()
 
 void loop()
 { 
+  float_msg.data = _LeftEncoderTicks;
+  encoder.publish(&float_msg);
   Serial.print("Encoder Ticks: ");
   Serial.print(_LeftEncoderTicks);
   Serial.print("  Revolutions: ");
-  Serial.print(_LeftEncoderTicks/4000.0);//4000 Counts Per Revolution
+  Serial.print(_LeftEncoderTicks/8192.0); //8192 Counts Per Revolution
   Serial.print("\n");
+  nh.spinOnce();
 }
 
 
